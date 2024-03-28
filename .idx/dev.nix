ba56dc22a2eb91dba62467ccda63085cfc924367
mohdumair.a <mohdumair.a@somaiya.edu>
2024-03-28 07:00:38 +0000

    ml-bootcamp

---
+++ .idx/dev.nix
@@ -0,0 +1,31 @@
+{pkgs}: {
+  channel = "stable-23.11";
+  packages = [
+    pkgs.nodejs_20
+  ];
+  idx.extensions = [
+    
+  ];
+  idx.previews = {
+    previews = [
+      {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--hostname"
+          "0.0.0.0"
+        ];
+        id = "web";
+        manager = "web";
+      }
+      {
+        id = "ios";
+        manager = "ios";
+      }
+    ];
+  };
+}
